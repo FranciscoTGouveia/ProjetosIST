d5aa6587e68440df5cba2fa08fae511ee8e40f2d
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-01-10 23:14:28 +0000

    Add predicate extrai_ilhas_linha/3

---
+++ 1ºAno/Lógica para a Programação/Projeto/projeto.pl
@@ -0,0 +1,13 @@
+% Predicado extrai_ilhas_linha
+extrai_ilhas_linha(N_L, Linha, Ilhas) :- extrai_ilhas_linha(N_L, Linha, [], Ilhas, 1).
+extrai_ilhas_linha(N_L, [], Ilhas, Ilhas).
+
+extrai_ilhas_linha(N_L, [Cabeca | Resto], Acc, Ilhas, Index) :-
+    Cabeca > 0,
+    append(Acc, ilha(Cabeca, (N_L, Index)), NovoAcc),
+    NovoIndex is Index + 1,
+    extrai_ilhas_linha(N_L, Resto, NovoAcc, Ilhas, No).
+
+extrai_ilhas_linha(N_L, [Cabeca | Resto], ListaFinal, Ilhas, Index) :-
+    NovoIndex is Index + 1,
+    extrai_ilhas_linha(N_L, Resto, ListaFinal, Ilhas, NovoIndex).
