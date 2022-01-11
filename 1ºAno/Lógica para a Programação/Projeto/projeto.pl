7dd7b4c96a5f616307efada8efe7329e1db5e2b8
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-01-11 15:32:21 +0000

    Fix extrai_ilhas_linha/3 predicate (Add extra argument in base case)

--- 1ºAno/Lógica para a Programação/Projeto/projeto.pl
+++ 1ºAno/Lógica para a Programação/Projeto/projeto.pl
@@ -1,14 +1,14 @@
 % Predicado extrai_ilhas_linha
 extrai_ilhas_linha(N_L, Linha, Ilhas) :- extrai_ilhas_linha(N_L, Linha, [], Ilhas, 1).
-extrai_ilhas_linha(N_L, [], Ilhas, Ilhas).
+extrai_ilhas_linha(N_L, [], Ilhas, Ilhas, _).
 
 extrai_ilhas_linha(N_L, [Cabeca | Resto], Acc, Ilhas, Index) :-
     Cabeca > 0, !,
-    append(Acc, ilha(Cabeca, (N_L, Index)), NovoAcc),
+    append(Acc, [ilha(Cabeca, (N_L, Index))], NovoAcc),
     NovoIndex is Index + 1,
-    extrai_ilhas_linha(N_L, Resto, NovoAcc, Ilhas, No).
+    extrai_ilhas_linha(N_L, Resto, NovoAcc, Ilhas, NovoIndex).
 
-extrai_ilhas_linha(N_L, [Cabeca | Resto], ListaFinal, Ilhas, Index) :-
+extrai_ilhas_linha(N_L, [_Cabeca | Resto], Acc, Ilhas, Index) :-
     NovoIndex is Index + 1,
-    extrai_ilhas_linha(N_L, Resto, ListaFinal, Ilhas, NovoIndex).
+    extrai_ilhas_linha(N_L, Resto, Acc, Ilhas, NovoIndex).
 
