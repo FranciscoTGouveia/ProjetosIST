5ee446e6401d4762b784bb83b7ecddf62f7178ee
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-01-11 23:39:39 +0000

    Add predicate ilhas/2

--- 1ºAno/Lógica para a Programação/Projeto/projeto.pl
+++ 1ºAno/Lógica para a Programação/Projeto/projeto.pl
@@ -1,9 +1,10 @@
+:- ['codigo_comum.pl', 'puzzles_publicos.pl'].
 % Predicado: extrai_ilhas_linha/3
 /* Objetivo: Transforma uma linha correspondente ao puzzle, numa lista
    com o numero de pontes de uma linha e a sua posicao no puzzle.
 */
-extrai_ilhas_linha(N_L, Linha, Ilhas) :- extrai_ilhas_linha(N_L, Linha, [], Ilhas, 1).
-extrai_ilhas_linha(N_L, [], Ilhas, Ilhas, _).
+extrai_ilhas_linha(N_L, Linha, Ilhas) :- extrai_ilhas_linha(N_L, Linha, [], Ilhas, 1). % Engorda do predicado
+extrai_ilhas_linha(_N_L, [], Ilhas, Ilhas, _). % Condicao de paragem
 
 extrai_ilhas_linha(N_L, [Cabeca | Resto], Acc, Ilhas, Index) :-
     Cabeca > 0, !,
@@ -15,3 +16,17 @@ extrai_ilhas_linha(N_L, [_Cabeca | Resto], Acc, Ilhas, Index) :-
     NovoIndex is Index + 1,
     extrai_ilhas_linha(N_L, Resto, Acc, Ilhas, NovoIndex).
 
+
+
+% Predicado: ilhas/2
+/* Objetivo: Devolve uma lista com todos os predicados ilha/2,
+   ordenados por posicao no puzzle.
+*/
+ilhas(Puz, Ilhas) :- ilhas(Puz, [], Ilhas, 1). % Engorda do predicado
+ilhas([], Ilhas, Ilhas, _). % Condicao de paragem
+
+ilhas([Cabeca | Resto], Acc, Ilhas, Index) :-
+    extrai_ilhas_linha(Index, Cabeca, Linha_Limpa),
+    append(Acc, Linha_Limpa, NovoAcc),
+    NovoIndex is Index + 1,
+    ilhas(Resto, NovoAcc, Ilhas, NovoIndex).
