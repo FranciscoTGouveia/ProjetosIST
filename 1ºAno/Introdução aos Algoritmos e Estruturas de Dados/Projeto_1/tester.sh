ae445df634f5f2be2326b030b3332dff72f2332c
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-19 00:07:56 +0000

    Add test script for Projeto_1

---
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/tester.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+cd tests/ || exit
+# Pass the tests to the algorithm
+mkdir eheh
+for test in *.in
+do
+	finaltest=$(echo $test | cut -c1-6)	
+	../main < "$test" > eheh/"$finaltest".out
+done
+
+# Compare the tests to the Solved_Tests
+for out in *.out
+do
+    if cmp -s "$out" "./eheh/$out"
+    then 
+        echo -e "\e[1;32m☑ Test $out Passed !\e[0m"
+    else
+        echo -e "\e[1;31m☒ Test $out Failed !\e[0m"
+    fi
+done
+
+# Remove clutter
+rm eheh/ -R
