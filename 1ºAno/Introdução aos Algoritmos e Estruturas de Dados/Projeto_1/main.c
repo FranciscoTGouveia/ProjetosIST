9a5caf2259a8b45b40d002830271a99f445a50a3
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-20 23:58:21 +0000

    Prepare main.c to receive commands

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/main.c
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/main.c
@@ -1,3 +1,25 @@
 #include <stdio.h>
+#include "actions.h"
 
-int main() { return 0; }
+int main() {
+    char action;
+    switch (action) {
+        case 'q':
+            return 0;
+        case 'a':
+            return 0;
+        case 'l':
+            return 0;
+        case 'v':
+            return 0;
+        case 'p':
+            return 0;
+        case 'c':
+            return 0;
+        case 't':
+            return 0;
+        default:
+            printf("Indisponível!\n");
+    }
+    return 0;
+}
