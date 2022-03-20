086bc3e814644e1c60580ccb9d5cca534e3a6382
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-20 23:59:40 +0000

    Add airports.c to tester script

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/tester.sh
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile the program
-gcc -ansi -pedantic -Wall -Wextra -Wall -o main main.c
+gcc -ansi -pedantic -Wall -Wextra -Wall -o main main.c airports.c
 
 cd tests/ || exit
 # Pass the tests to the algorithm
