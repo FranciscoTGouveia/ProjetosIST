e472d6b7cf83cb571d854e01fa54a556cad4e591
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-22 18:28:00 +0000

    Add vector of structs and error handling (check_invalid_id and check_2_many)

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/add.c
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/add.c
@@ -1,25 +1,58 @@
-#include "actions.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "actions.h"
 
-Airport create_airport(char id[], char country[], char city[]) {
-    Airport airport;
-    strcpy(airport.id, id);
-    strcpy(airport.country, country);
-    strcpy(airport.city, city);
-    return airport;
+static void add_airport_to_vector(char id[], char country[], char city[],
+                                  Airport airports_vector[],
+                                  int airports_counter) {
+
+    strcpy(airports_vector[airports_counter].id, id);
+    strcpy(airports_vector[airports_counter].country, country);
+    strcpy(airports_vector[airports_counter].city, city);
 }
 
-void print_new_airport(Airport airport) {
-    printf("airport %s\n", airport.id);
+static void print_airport_id(Airport airports_vector[],
+                             int airports_counter) {
+    printf("airport %s\n", airports_vector[airports_counter].id);
 }
 
-void new_airport() {
+/* Check if an ID is valid (all upper letters) */
+static int check_invalid_id(char id[]) {
+    int i;
+    for (i = 0; i < MAX_ID; i++) {
+        if (!isupper(id[i])) {
+            printf("invalid airport ID\n");
+            return 1;
+        }
+    }
+    return 0;
+}
+
+/* Check if the new airport will not exceed the limit */
+static int check_2_many(int airports_counter) {
+    if (airports_counter >= MAX_AIRPORTS) {
+        printf("too many airports\n");
+        return 1;
+    }
+    return 0;
+}
+
+int new_airport(Airport airports_vector[], int airports_counter) {
     char id[MAX_ID + 1];
     char country[MAX_COUNTRY + 1];
     char city[MAX_CITY + 1];
-    Airport new_airport;
-    scanf("%s %s %[^\n]", id, country, city);
-    new_airport = create_airport(id, country, city);
-    print_new_airport(new_airport);
+    int status_id, status_2_many;
+    scanf("%s %s %[^\n]s", id, country, city);
+    status_id = check_invalid_id(id);
+    status_2_many = check_2_many(airports_counter);
+    /*check_duplicate();*/
+    if (status_id || status_2_many) {
+        return 1;
+    }
+    add_airport_to_vector(id, country, city, airports_vector,
+                          airports_counter);
+    print_airport_id(airports_vector, airports_counter);
+    return 0;
 }
