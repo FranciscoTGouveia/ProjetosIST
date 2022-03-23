a3ca4648537747ee73b371372bec916f91ea9745
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-23 18:10:12 +0000

    Add Flight-related defines and fix ERR_NO_SUCH_AIRPORT_ID

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/commands.h
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/commands.h
@@ -7,14 +7,14 @@
 #include <ctype.h>
 #include <string.h>
 
-
 /* Type related constants */
+/* Airport related */
 #define MAX_ID 3
 #define MAX_COUNTRY 30
 #define MAX_CITY 50
 #define MAX_AIRPORTS 40
-
-
+/* Flight related */
+#define MAX_FLIGHT_CODE 6
 
 /* Available commands keys */
 #define QUIT_CMD 'q'
@@ -25,15 +25,11 @@
 #define LIST_ARRIVALS_CMD 'c'
 #define STEP_TIME_CMD 't'
 
-
-
 /* User-Oriented messages */
 #define ERR_INVALID_AIRPORT_ID "invalid airport ID\n"
 #define ERR_TOO_MANY_AIRPORTS "too many airports\n"
 #define ERR_DUPLICATE_AIRPORT "duplicate airport\n"
-#define ERR_NO_SUCH_AIRPORT_ID "<IDAeroporto>: no such airport ID\n"
-
-
+#define ERR_NO_SUCH_AIRPORT_ID "no such airport ID\n"
 
 /* Custom Types (Structures) */
 typedef struct {
@@ -42,12 +38,19 @@ typedef struct {
     char city[MAX_CITY + 1];
 } Airport;
 
-
+typedef struct {
+    char flight_code[MAX_FLIGHT_CODE + 1];
+    char id_departure[MAX_ID + 1];
+    char id_arrival[MAX_ID + 1];
+    /* data: */
+    short duration;
+    short capacity;
+} Flight;
 
 /* "Add" command function: */
 int new_airport(Airport airports_vector[], int airports_counter);
 
 /* "List" command function */
-void list_airports();
+int list_airports();
 
 #endif
