07a7e5b168bbe5fcdb9dd3a7ff728067ac3e00e2
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-25 16:58:24 +0000

    Add flight related symbolic constants

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/commands.h
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/commands.h
@@ -15,6 +15,9 @@
 #define MAX_AIRPORTS 40
 /* Flight related */
 #define MAX_FLIGHT_CODE 6
+#define MAX_FLIGHTS 30000
+#define MAX_DATE 10
+#define MAX_TIME 5
 
 /* Available commands keys */
 #define QUIT_CMD 'q'
@@ -23,13 +26,14 @@
 #define ADD_FLIGHT_CMD 'v'
 #define LIST_DEPARTURES_CMD 'p'
 #define LIST_ARRIVALS_CMD 'c'
-#define STEP_TIME_CMD 't'
+#define STEP_DATE_CMD 't'
 
 /* User-Oriented messages */
 #define ERR_INVALID_AIRPORT_ID "invalid airport ID\n"
 #define ERR_TOO_MANY_AIRPORTS "too many airports\n"
 #define ERR_DUPLICATE_AIRPORT "duplicate airport\n"
 #define ERR_NO_SUCH_AIRPORT_ID "no such airport ID\n"
+#define ERR_INVALID_FLIGHT_CODE "invalid flight code\n"
 
 /* Custom Types (Structures) */
 typedef struct {
@@ -42,15 +46,20 @@ typedef struct {
     char flight_code[MAX_FLIGHT_CODE + 1];
     char id_departure[MAX_ID + 1];
     char id_arrival[MAX_ID + 1];
-    /* data: */
+    char date[MAX_DATE + 1];
+    char time[MAX_TIME + 1];
     short duration;
     short capacity;
 } Flight;
 
-/* "Add" command function: */
+/* "Add Airport" command function: */
 int new_airport(Airport airports_vector[], int airports_counter);
+void sort_airports(Airport airports_vector[], int airports_counter);
+int new_flight(Flight flight_vec[], int flight_count);
 
 /* "List" command function */
 int list_airports();
+
+/* "Add Flight" command function */
 
 #endif
