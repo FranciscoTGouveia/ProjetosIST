b5c2f77af29c9dc9a5f642db3684302d2cb7ea42
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-26 18:09:53 +0000

    Add 'same_day' function

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/date.c
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/date.c
@@ -9,7 +9,17 @@
 
 int time2int(int hours, int minutes) { return (hours * 60) + minutes; }
 
-static int check_date(int last_date, int new_date) {
+int same_day(int date, int day, int month, int year) {
+    int date_day = (date & 0xff);
+    int date_month = ((date >> 8) & 0xff);
+    int date_year = ((date >> 16) & 0xffff);
+    if (date_day == day && date_month == month && date_year == year) {
+        return 1;
+    }
+    return 0;
+}
+
+int check_date(int last_date, int new_date) {
     /* When transformed to an int, a 1 year difference is 65536 */
     if (new_date < last_date || (new_date - last_date) > 65536) {
         return 0;
