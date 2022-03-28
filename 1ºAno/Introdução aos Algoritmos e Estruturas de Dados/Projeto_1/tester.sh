78328b0b4ffb487002a55a5784c34f35e3e281a3
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-28 10:13:46 +0100

    Add tests and update tester.sh

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/tester.sh
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/tester.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Compile the program
-gcc -ansi -pedantic -Wall -Wextra -Wall -o main main.c add.c listAirports.c flight.c date.c
+gcc -ansi -pedantic -Wall -Wextra -Wall -o main main.c add.c listAirports.c flight.c date.c listDepartures.c listArrivals.c
 
 cd tests/ || exit
 # Pass the tests to the algorithm
