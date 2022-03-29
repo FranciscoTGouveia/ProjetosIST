63a2f56a12ebabdd13179481cd44a6572b8074d5
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-29 08:15:21 +0100

    Clean add.c file

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/add.c
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/add.c
@@ -3,14 +3,15 @@
 static void add_airport_to_vector(char id[], char country[], char city[],
                                   Airport airports_vector[],
                                   int airports_counter) {
-
+    /* Add new airport to airports vector */
     strcpy(airports_vector[airports_counter].id, id);
     strcpy(airports_vector[airports_counter].country, country);
     strcpy(airports_vector[airports_counter].city, city);
 }
 
-/* Check if an ID is valid (all upper letters) */
+
 static int check_invalid_id(char id[]) {
+    /* Check if an ID is valid (all upper letters) */
     int i;
     for (i = 0; i < MAX_ID; i++) {
         if (!isupper(id[i])) {
@@ -21,17 +22,20 @@ static int check_invalid_id(char id[]) {
     return 1;
 }
 
-/* Check if the new airport will not exceed the limit */
+
 static int check_2_many_airports(int airports_counter) {
-    if (airports_counter >= MAX_AIRPORTS) {
+    /* Check if the new airport will not exceed the limit */    
+    if (airports_counter >= MAX_AIRPORTS) { 
         printf(ERR_TOO_MANY_AIRPORTS);
         return 0;
     }
     return 1;
 }
 
+
 static int check_duplicates(Airport airports_vector[], char id[],
                             int airports_counter) {
+    /* Check if there isn't already an airport with the same ID */ 
     int i;
     for (i = 0; i < airports_counter; i++) {
         if (strcmp(airports_vector[i].id, id) == 0) {
@@ -42,15 +46,14 @@ static int check_duplicates(Airport airports_vector[], char id[],
     return 1;
 }
 
+
 static int check_errors_new_airport(Airport airports_vector[],
                                     int airports_counter, char id[]) {
-    int status_id, status_2_many, status_duplicate;
-    status_id = check_invalid_id(id);
-    status_2_many = check_2_many_airports(airports_counter);
-    status_duplicate =
-        check_duplicates(airports_vector, id, airports_counter);
-    return (status_id && status_2_many && status_duplicate) ? 1 : 0;
+    /* Agreggate all error-checking in a single function */ 
+    return (check_invalid_id(id) && check_2_many_airports(airports_counter) &&
+                    check_duplicates(airports_vector, id, airports_counter));
 }
+
 
 void sort_airports(Airport air_vec[], int vec_size) {
     /* Sort airports by ID (bubble sort) */
@@ -67,12 +70,13 @@ void sort_airports(Airport air_vec[], int vec_size) {
     }
 }
 
+
 int new_airport(Airport airports_vector[], int airports_counter) {
     char id[MAX_ID + 1];
     char country[MAX_COUNTRY + 1];
     char city[MAX_CITY + 1];
     scanf("%s %s %[^\n]s", id, country, city);
-
+    /* Check errors and add airport to airports vector */
     if (check_errors_new_airport(airports_vector, airports_counter, id)) {
         add_airport_to_vector(id, country, city, airports_vector,
                               airports_counter);
