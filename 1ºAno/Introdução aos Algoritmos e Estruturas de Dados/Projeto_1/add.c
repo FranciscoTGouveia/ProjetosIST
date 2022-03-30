4e5c4b659e3ab27730de080c44ce0d45d915a43e
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-30 11:23:38 +0100

    Change sorting algorithm to Insertion Sort

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/add.c
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/add.c
@@ -9,7 +9,6 @@ static void add_airport_to_vector(char id[], char country[], char city[],
     strcpy(airports_vector[airports_counter].city, city);
 }
 
-
 static int check_invalid_id(char id[]) {
     /* Check if an ID is valid (all upper letters) */
     int i;
@@ -22,17 +21,15 @@ static int check_invalid_id(char id[]) {
     return 1;
 }
 
-
 static int check_2_many_airports(int air_count) {
     /* Check if the new airport will not exceed the limit */
     if (air_count >= MAX_AIRPORTS) printf(ERR_TOO_MANY_AIRPORTS);
     return (air_count < MAX_AIRPORTS);
 }
 
-
 static int check_duplicates(Airport airports_vector[], char id[],
                             int airports_counter) {
-    /* Check if there isn't already an airport with the same ID */ 
+    /* Check if there isn't already an airport with the same ID */
     int i;
     for (i = 0; i < airports_counter; i++) {
         if (strcmp(airports_vector[i].id, id) == 0) {
@@ -43,41 +40,37 @@ static int check_duplicates(Airport airports_vector[], char id[],
     return 1;
 }
 
-
 static int check_errors_new_airport(Airport airports_vector[],
                                     int airports_counter, char id[]) {
-    /* Agreggate all error-checking in a single function */ 
+    /* Agreggate all error-checking in a single function */
     return (check_invalid_id(id) && check_2_many_airports(airports_counter) &&
-                    check_duplicates(airports_vector, id, airports_counter));
+            check_duplicates(airports_vector, id, airports_counter));
 }
 
-
 void sort_airports(Airport air_vec[], int vec_size) {
-    /* Sort airports by ID (bubble sort) */
+    /* Sort airports by ID (insertion sort) */
     int i, j;
     Airport temp;
-    for (i = 0; i < (vec_size - 1); i++) {
-        for (j = 0; j < (vec_size - i - 1); j++) {
-            if (strcmp(air_vec[j + 1].id, air_vec[j].id) < 0) {
-                temp = air_vec[j];
-                air_vec[j] = air_vec[j + 1];
-                air_vec[j + 1] = temp;
-            }
+    for (i = 1; i < vec_size; i++) {
+        temp = air_vec[i];
+        j = i - 1;
+        while (j >= 0 && strcmp(air_vec[j].id, temp.id) > 0) {
+            air_vec[j + 1] = air_vec[j];
+            j--;
         }
+        air_vec[j + 1] = temp;
     }
 }
 
-
-int new_airport(Airport airports_vector[], int airports_counter) {
+int new_airport(Airport air_vec[], int air_count) {
     char id[MAX_ID + 1];
     char country[MAX_COUNTRY + 1];
     char city[MAX_CITY + 1];
     scanf("%s %s %[^\n]s", id, country, city);
     /* Check errors and add airport to airports vector */
-    if (check_errors_new_airport(airports_vector, airports_counter, id)) {
-        add_airport_to_vector(id, country, city, airports_vector,
-                              airports_counter);
-        printf("airport %s\n", airports_vector[airports_counter].id);
+    if (check_errors_new_airport(air_vec, air_count, id)) {
+        add_airport_to_vector(id, country, city, air_vec, air_count);
+        printf("airport %s\n", air_vec[air_count].id);
         return 1;
     }
     return 0;
