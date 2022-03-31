db293a6ec5f07587f8e106f6450ff51be15f971c
Francisco Gouveia <francisco.t.gouveia@tecnico.ulisboa.pt>
2022-03-31 11:23:01 +0100

    Final version, respecting 25 line limits (Full mark on Mooshak)

--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/commands.h
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/commands.h
@@ -58,17 +58,14 @@ typedef struct {
 int new_airport(Airport airports_vector[], int airports_counter);
 void sort_airports(Airport airports_vector[], int airports_counter);
 
-
 /* "List" command function: */
 void list_airports(Airport airports_vector[], int airports_counter,
                    Flight flight_vec[], int flight_count);
-
 
 /* "Add Flight" command function: */
 int new_flight(Airport air_vec[], int air_count, Flight flight_vec[],
                int flight_count, int date);
 int binary_search(Airport air_vec[], char target[], int l, int r);
-
 
 /* "List departures" command function */
 void list_departures(Airport air_vec[], int air_count, Flight flight_vec[],
@@ -79,7 +76,6 @@ void list_arrivals(Airport air_vec[], int air_count, Flight flight_vec[],
                    int flight_count);
 int calculate_arrival_time(int arr_time);
 int calculate_arrival_date(int arr_time, int date);
-
 
 /* "Set time" command functions: */
 int step_date(int last_date);
--- 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/main.c
+++ 1ºAno/Introdução aos Algoritmos e Estruturas de Dados/Projeto_1/main.c
@@ -1,44 +1,44 @@
 #include "commands.h"
 
+static void execute(char action, Airport air_vec[], int *air_count,
+                    Flight flights_vec[], int *flights_count, int *date) {
+    switch (action) {
+        case ADD_AIRPORT_CMD:
+            if (new_airport(air_vec, *air_count)) {
+                (*air_count)++;
+                sort_airports(air_vec, *air_count);
+            }
+            break;
+        case LIST_AIRPORTS_CMD:
+            list_airports(air_vec, *air_count, flights_vec, *flights_count);
+            break;
+        case ADD_FLIGHT_CMD:
+            if (new_flight(air_vec, *air_count, flights_vec, *flights_count,
+                           *date))
+                (*flights_count)++;
+            break;
+        case LIST_DEPARTURES_CMD:
+            list_departures(air_vec, *air_count, flights_vec, *flights_count);
+            break;
+        case LIST_ARRIVALS_CMD:
+            list_arrivals(air_vec, *air_count, flights_vec, *flights_count);
+            break;
+        case STEP_DATE_CMD:
+            *date = step_date(*date);
+            break;
+    }
+}
+
 int main() {
     char action;
-    Airport airports_vector[MAX_AIRPORTS];
-    int airports_counter = 0;
-    Flight flights_vector[MAX_FLIGHTS];
-    int flights_counter = 0;
+    Airport air_vec[MAX_AIRPORTS];
+    Flight flights_vec[MAX_FLIGHTS];
+    int air_count = 0, flights_count = 0;
     int date = date2int(1, 1, 2022);
 
-    while ((action = getchar()) != EOF) {
-        switch (action) {
-            case QUIT_CMD:
-                return 0;
-            case ADD_AIRPORT_CMD:
-                if (new_airport(airports_vector, airports_counter)) {
-                    airports_counter++;
-                    sort_airports(airports_vector, airports_counter);
-                }
-                break;
-            case LIST_AIRPORTS_CMD:
-                list_airports(airports_vector, airports_counter,
-                              flights_vector, flights_counter);
-                break;
-            case ADD_FLIGHT_CMD:
-                if (new_flight(airports_vector, airports_counter,
-                               flights_vector, flights_counter, date))
-                    flights_counter++;
-                break;
-            case LIST_DEPARTURES_CMD:
-                list_departures(airports_vector, airports_counter,
-                                flights_vector, flights_counter);
-                break;
-            case LIST_ARRIVALS_CMD:
-                list_arrivals(airports_vector, airports_counter,
-                              flights_vector, flights_counter);
-                break;
-            case STEP_DATE_CMD:
-                date = step_date(date);
-                break;
-        }
+    while ((action = getchar()) != EOF && action != QUIT_CMD) {
+        execute(action, air_vec, &air_count, flights_vec, &flights_count,
+                &date);
     }
     return 0;
 }
